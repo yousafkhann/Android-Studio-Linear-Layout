138458c126588cb902d4fe6bebcad58c967aa7ad
Yousaf Khan <yousaf.khan619@gmail.com>
2022-06-02 02:49:16 -0500

    File Upload

---
+++ app/src/main/java/com/yousafkhan/homework2/TipActivity.java
@@ -0,0 +1,14 @@
+package com.yousafkhan.homework2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TipActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tip);
+    }
+}
